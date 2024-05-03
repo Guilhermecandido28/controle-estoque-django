6a8f2f3d259624571e9bbec6754d3354a544f79c
Guilhermecandido28 <gnfc28@icloud.com>
2024-05-03 15:33:33 -0300

    correção do gráfico vendas

--- templates/vendas/static/js/venda.js
+++ templates/vendas/static/js/venda.js
@@ -1,46 +1,66 @@
-  function renderiza_grafico1(){
-    const ctx = document.getElementById('dashbord1').getContext('2d');
-    
-    // Defina as cores personalizadas para cada categoria
-    const cores = {
-        'Crédito': 'blue',
-        'Débito': 'green',
-        'Pix': 'orange',
-        'Dinheiro': 'red'
-    };
-    
-    // Obtenha as chaves(labels) do objeto cores
-    const labels = Object.keys(cores);
-    
-    //Cria o grafico
-    const myChart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Vendas',
-          data: [499.15, 600, 325.37, 432],
-          backgroundColor: labels.map(label => cores[label]), // Mapeie as cores de acordo com as labels
-          borderColor: 'black', // cor do contorno das barras
-          borderWidth: 1,
-          borderRadius: 15,
-          inflateAmount: 0.2,          
-        }]
-      },
-      options: {
-        plugins: {
-            title:{
-                display: true,
-                text: "Vendas R$ x Categoria",                
-                color:'black'            
-            },
-           legend: {
-            display: true,            
-           } 
-        }
-      }        
-    });
+function renderiza_grafico1() {
+  const ctx = document.getElementById('dashbord1').getContext('2d');
+
+  // Realiza uma solicitação AJAX para obter os dados da movimentação do dia
+  fetch('movimentacao_dia')
+      .then(response => response.json())
+      .then(data => {
+          const cores = {
+              'CARTAO': 'blue',
+              'DEBITO': 'green',
+              'PIX': 'orange',
+              'DINHEIRO': 'red'
+          };
+
+          // Extrai as formas de pagamento e os totais de vendas do retorno da API
+          const formas_pagamento = {};
+          const totais_vendas = {};
+          data.forEach(item => {
+              formas_pagamento[item.forma_pagamento] = true;
+              if (!totais_vendas[item.forma_pagamento]) {
+                  totais_vendas[item.forma_pagamento] = 0;
+              }
+              totais_vendas[item.forma_pagamento] += parseFloat(item.soma_total);
+          });
+
+          // Converte os objetos em arrays para serem usados no gráfico
+          const labels = Object.keys(formas_pagamento);
+          const valores = labels.map(label => totais_vendas[label]);
+
+          // Cria o gráfico
+          const myChart = new Chart(ctx, {
+              type: 'pie',
+              data: {
+                  labels: labels,
+                  datasets: [{
+                      label: 'Vendas',
+                      data: valores,
+                      backgroundColor: labels.map(label => cores[label]),
+                      borderColor: 'black',
+                      borderWidth: 1,
+                      borderRadius: 15,
+                      inflateAmount: 0.2,
+                  }]
+              },
+              options: {
+                  plugins: {
+                      title: {
+                          display: true,
+                          text: "Vendas R$ x Forma de Pagamento",
+                          color: 'black'
+                      },
+                      legend: {
+                          display: true,
+                      }
+                  }
+              }
+          });
+      })
+      .catch(error => {
+          console.error('Ocorreu um erro ao buscar os dados de movimentação do dia:', error);
+      });
 }
+
 
 // Função para calcular o preço total para uma linha da tabela
 function calcularPrecoTotal(row) {
@@ -61,7 +81,7 @@ function calcularPrecoTotal(row) {
   return precoTotal.toFixed(2); // Arredonda para 2 casas decimais
 }
 
-// Função para atualizar o total geral na página
+// Função para atualizar o total geral na tabela
 function atualizarTotalGeral() {
   // console.log('Início da atualização do total geral');
   var linhas = document.querySelectorAll('#lista_item tbody tr');
@@ -138,4 +158,36 @@ document.body.addEventListener('htmx:afterSwap', function(event) {
 });
 
 
+function deleteRows(){
+  var tabela = document.getElementById("lista_item");
+  var rowCount = tabela.rows.length;  
+  for (var i = rowCount - 1; i > 0; i--){
+    tabela.deleteRow(i);
+  }
+  
+  atualizarTotalGeral()
+}
 
+// função que atualiza o total quando é colocado um desconto
+function calcularNovoTotal(desconto) {
+  // Obter o total original da venda
+  var totalOriginal = parseFloat(document.getElementById('total_geral').textContent.replace('R$ ', ''));
+  // Calcular o novo total com base no desconto
+  var descontoValor = (totalOriginal * desconto) / 100;
+  var novoTotal = totalOriginal - descontoValor;
+  // Atualizar o total no modal
+  atualizarTotalModal(novoTotal);
+}
+
+// Adicionar um evento ao input de desconto para capturar quando o valor for alterado
+var inputDesconto = document.getElementById('id_desconto');
+if (inputDesconto) {
+  inputDesconto.addEventListener('input', function(event) {
+      // Obter o valor do desconto inserido pelo usuário
+      var desconto = parseFloat(event.target.value);
+      if (!isNaN(desconto)) {
+          // Calcular o novo total da venda com base no desconto
+          calcularNovoTotal(desconto);
+      }
+  });
+}
