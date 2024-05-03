d37d8e3ac3eaace1e4311b41dea9f92ae682aa30
Guilhermecandido28 <gnfc28@icloud.com>
2024-05-03 15:31:04 -0300

    tag customizada da venda

--- custom_tags/custom_filters.py
+++ custom_tags/custom_filters.py
@@ -13,5 +13,17 @@ def multiply(value, arg):
 
 @register.filter(name='currency')
 def currency(value):
-    return locale.currency(value)
+    return locale.currency(float(value))
 
+@register.filter(name='venda')
+def vis_venda(string):
+    print(string)
+    sem_parenteses = string.strip('()')
+    
+    elementos = sem_parenteses.split(',')
+    elementos_sem_aspas = [elemento.strip().strip("'") for elemento in elementos]
+        
+    output = ""    
+    output += f"{elementos_sem_aspas[1]}, {elementos_sem_aspas[2]}, {elementos_sem_aspas[3].replace("'",'').replace(')','')} \n"    
+    output += f", {elementos_sem_aspas[0]}. \n"    
+    return output
