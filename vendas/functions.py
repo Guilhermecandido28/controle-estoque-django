44c9ef7c395d09abb2d5ebb198b119eae2353664
Guilhermecandido28 <gnfc28@icloud.com>
2024-05-03 15:33:57 -0300

    função que subtrai um do estoque

---
+++ vendas/functions.py
@@ -0,0 +1,11 @@
+from estoque.models import Estoque
+
+def ajustar_estoque(lista):
+    for item in lista:
+        codigo_barras = item['codigo_barras']
+        estoque_item = Estoque.objects.filter(codigo_barras=codigo_barras).first()
+        if estoque_item:
+            estoque_item.quantidade -= 1
+            estoque_item.save()
+        else:
+            print(f"O item com código de barras {codigo_barras} não está presente no estoque.")
