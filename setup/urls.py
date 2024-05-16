bccce13105c8cf0d23e086d5310a5abee3a1b322
Guilhermecandido28 <gnfc28@icloud.com>
2024-05-16 17:37:30 -0300

    INCLUDE DAS URLS DO APP VENDEDORES

--- setup/urls.py
+++ setup/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('', include('login.urls')),
     path('estoque/', include('estoque.urls')),
     path('clientes/', include('clientes.urls')),
-    path('vendas/', include('vendas.urls')),    
+    path('vendas/', include('vendas.urls')),
+    path('vendedores/', include('vendedores.urls')),    
     path("__debug__/", include("debug_toolbar.urls")),
 ]
