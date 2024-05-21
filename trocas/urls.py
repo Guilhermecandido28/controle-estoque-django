06f9cdffe4f5d532350ff78c56b16acff9ab79a6
Guilhermecandido28 <gnfc28@icloud.com>
2024-05-21 19:29:09 -0300

    app trocas

--- trocas/urls.py
+++ trocas/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.trocas, name='trocas'),   
+    path('', views.trocas, name='trocas'),
+    path('pesquisa_por_vendedor', views.pesquisa_por_vendedor, name='pesquisa_por_vendedor'),
+    path('pesquisa_por_cliente', views.pesquisa_por_cliente_e_venda, name='pesquisa_por_cliente_e_venda'), 
+      
 
 ]
--- trocas/views.py
+++ trocas/views.py
@@ -1,4 +1,63 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .filters import TrocaVendedorFilter, TrocaClienteFilter, TrocaVendaFilter
+from vendedores.models import EventoVenda
+from vendas.models import Vendas
+
 
 def trocas(request):
-    return HttpResponse('trocas')
+    template_name = 'trocas/trocas.html'
+    filtro_vendedor = TrocaVendedorFilter()
+    filtro_cliente = TrocaClienteFilter()
+    filtro_venda = TrocaVendaFilter()
+    context={'filtro_vendedor': filtro_vendedor, 'filtro_cliente': filtro_cliente, 'filtro_venda': filtro_venda}
+    return render(request, template_name , context )
+
+
+def pesquisa_por_vendedor(request):        
+    filtro = TrocaVendedorFilter(request.POST, queryset=EventoVenda.objects.all())    
+    object_vendedor = filtro.qs
+    context = {
+        'object_vendedor': object_vendedor,
+        'filtro': filtro,
+        'object_cliente': [],              
+    }    
+    template_name = 'trocas/partials/_tabela_vendedor.html'    
+    return render(request, template_name, context)
+    
+
+def pesquisa_por_cliente_e_venda(request):       
+    # Imprimindo os dados recebidos na requisição POST
+    print("Dados da requisição POST:", request.POST)
+    
+    # Criação do filtro com os dados da requisição e queryset inicial
+    filtro = TrocaVendedorFilter(request.POST, queryset=Vendas.objects.all())    
+    
+    # Imprimindo o queryset inicial
+    print("Queryset inicial (Vendas.objects.all()):", Vendas.objects.all())
+    
+    # Aplicação do filtro e obtenção do queryset filtrado
+    object_cliente_e_venda = filtro.qs
+    
+    # Imprimindo o queryset após aplicação do filtro
+    print("Queryset após aplicação do filtro (filtro.qs):", object_cliente_e_venda)
+    
+    # Definindo o contexto a ser passado para o template
+    context = {
+        'object_vendedor': [],
+        'filtro': filtro,
+        'object_cliente_e_venda': object_cliente_e_venda,
+    }    
+    
+    # Imprimindo o contexto antes de renderizar o template
+    print("Contexto para o template:", context)
+    
+    template_name = 'trocas/partials/_tabela_cliente_venda.html'
+
+    # Renderizando o template com o contexto
+    return render(request, template_name, context)
+
+   
+
+
+    
+    
